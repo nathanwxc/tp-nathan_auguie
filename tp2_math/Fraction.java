e97a875698a4847a545956ee971abee8ad8a25c5
STEPHANE AUGUIE <nathanwx.auguie@gmail.com>
2025-06-12 16:02:14 +0200

    Fraction step 1 added

---
+++ tp2_math/Fraction.java
@@ -0,0 +1,18 @@
+public class Fraction {
+    private final int num;  // numérateur
+    private final int den;  // dénominateur (>0)
+    public Fraction(int num, int den) {
+        int pgcd = pgcd(int num, int den);
+        if (pgcd==1) {
+            this.num = num;
+            this.den = den;
+        } 
+        else {
+            this.num = num%pgcd;
+            this.den = den%pgcd;
+        }// TODO: simplifier la fraction avec pgcd
+        if (den==0){
+            throw new IllegalArgumentException("impossible to divide by 0");
+        }// TODO: gérer den == 0 (IllegalArgumentException)
+    }
+}
